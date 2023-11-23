2691918b69e59ffd2700b3749790799b65d4cca4
MDGHOUSE18 <mdghouse23102@gmail.com>
2023-11-23 15:19:35 +0530

    commit changes

--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -1,25 +1,27 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import {  RouterModule,Routes } from '@angular/router';
-import { AppComponent } from './app.component';
-import { HomeComponent } from './components/home/home.component';
-import { AddEmployeeComponent } from './components/add-employee/add-employee.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { HttpClientModule } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
-import { UpdateEmployeeComponent } from './components/update-employee/update-employee.component';
-import { AddDepartmentComponent } from './components/add-department/add-department.component';
-import { UpdateDepartmentComponent } from './components/update-department/update-department.component';
-
-
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { AppComponent } from "./app.component";
+import { HomeComponent } from "./components/home/home.component";
+import { AddEmployeeComponent } from "./components/add-employee/add-employee.component";
+import { NavbarComponent } from "./components/navbar/navbar.component";
+import { HttpClientModule } from "@angular/common/http";
+import { FormsModule } from "@angular/forms";
+import { UpdateEmployeeComponent } from "./components/update-employee/update-employee.component";
+import { AddDepartmentComponent } from "./components/add-department/add-department.component";
+import { UpdateDepartmentComponent } from "./components/update-department/update-department.component";
+import { ContactUsComponent } from "./components/contact-us/contact-us.component";
+import { AboutUsComponent } from "./components/about-us/about-us.component";
 
 const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'add-employee', component: AddEmployeeComponent },
-  { path: 'add-department', component: AddDepartmentComponent },
-  { path: 'employees/:id', component: UpdateEmployeeComponent },
-  { path: 'departments/:id', component: UpdateDepartmentComponent }
-  
+  { path: "", component: HomeComponent },
+  { path: "add-employee", component: AddEmployeeComponent },
+  { path: "add-department", component: AddDepartmentComponent },
+  { path: "employees/:id", component: UpdateEmployeeComponent },
+  { path: "departments/:id", component: UpdateDepartmentComponent },
+
+  { path: "contact", component: ContactUsComponent },
+  { path: "about", component: AboutUsComponent },
 ];
 @NgModule({
   declarations: [
@@ -29,12 +31,17 @@ const routes: Routes = [
     NavbarComponent,
     UpdateEmployeeComponent,
     AddDepartmentComponent,
-    UpdateDepartmentComponent
+    UpdateDepartmentComponent,
+    ContactUsComponent,
+    AboutUsComponent,
   ],
   imports: [
-    BrowserModule,RouterModule.forRoot(routes),HttpClientModule,FormsModule
+    BrowserModule,
+    RouterModule.forRoot(routes),
+    HttpClientModule,
+    FormsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
